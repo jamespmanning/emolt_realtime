40ac47e51c1b1934db7525f74b8aecbafabfaab7
Jim Manning <james.manning@noaa.gov>
2022-01-20 13:30:58 -0500

    deleted credentials

--- rtd_global.tar/rtd_global/config.py
+++ rtd_global.tar/rtd_global/config.py
@@ -4,10 +4,10 @@ database_connection_details = {
 'development':{
         'ssh_host': '54.183.67.0',
         'ssh_username': 'ec2-user',
-        'ssh_private_key': '/home/pi/rtd_global/BDC_Database/aws_ec2.pem',
+        'ssh_private_key': '',
         'ssh_db_host': '127.0.0.1',
         
-        'db_host': 'coast-data-network.cmcfqdpj50ec.us-west-1.rds.amazonaws.com',
+        'db_host': '',
         'db_port': 3306,
         'db_user': 'rw_user',
         'db_password': '3gJL*cgJu.{>"hmJ',
