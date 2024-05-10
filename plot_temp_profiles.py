a1a7a799b68597cf6660bc549738bc45ecd84113
jamespmanning <james.patrick.manning@gmail.com>
2024-05-10 12:30:38 -0400

    modified plt_temp_profiles to sort by latitude

--- plot_temp_profiles.py
+++ plot_temp_profiles.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan  2 17:21:46 2024
-derived from my circa 2021 "plot_ctd_sta_profiles.py"
+derived from "plot_ctd_sta_profiles.py" which plots NEFSC CTD data
 @author: JiM
 """
 
@@ -14,15 +14,37 @@ import netCDF4
 import ftplib
 import glob
 import os,imageio
+import conda 
+conda_file_dir = conda.__file__
+conda_dir = conda_file_dir.split('lib')[0]
+proj_lib = os.path.join(os.path.join(conda_dir, 'share'), 'proj')
+from mpl_toolkits.basemap import Basemap
+import warnings
+warnings.filterwarnings("ignore") # JiM added Sep 2020 to supopress the "matplotlibDeprecationWarning"
 # add some homegrown functions
 from conversions import c2f,m2fth
 
-mode='gif'
+#HARDCODES
 area='NE'
 st='Profiling%20Up' #segment_type
 startt=(dt.datetime.now()-dt.timedelta(days=30)).strftime('%Y-%m-%dT%H:%M:%SZ')#'2023-12-01T13:53:21Z'
 surf_or_bot='bottom'
+try:
+    url='http://www.smast.umassd.edu:8080/thredds/dodsC/fvcom/hindcasts/30yr_gom3'
+    nc = netCDF4.Dataset(url).variables
+    lats = nc['lat'][:]
+    lons = nc['lon'][:]
+    depths = nc['h'][:]  # depth
+    depths_down='no'
+except:
+    print('30yr_gom3 down')
+    depths_down='yes'
 
+# clean up the plots directory for this new run
+files = os.listdir("/home/user/sst/plots/")
+for file in files:
+    os.remove('/home/user/sst/plots/'+file) # delete all files
+    
 def eMOLT_cloud(ldata):# send file to SD machine where "ldata" is an array in bracketts like ['file1','file2']
         # function to upload a list of files to SD machine
         for filename in ldata:
@@ -65,7 +87,47 @@ def getgbox(area):
     gbox=[-76.,-66.,35.,44.5] # NE Shelf 
   return gbox
 
-def make_gif(gif_name,png_dir,frame_length = 0.2,end_pause = 4 ):
+def make_basemap(gbox,resolution='i'):
+    latsize=[gbox[2],gbox[3]]
+    lonsize=[gbox[0],gbox[1]]
+    tick_int=gbox[3]-gbox[2] # allow for 3-4 tick axis label intervals
+    if (tick_int>=1):
+        tick_int=1.   # make the tick_interval integer increments
+    elif (tick_int>.5) & (tick_int<1):
+        tick_int=.5
+    else:
+        tick_int=.2
+    
+    #fig,ax=plt.subplots()
+    m = Basemap(projection='merc',llcrnrlat=min(latsize),urcrnrlat=max(latsize),\
+                llcrnrlon=min(lonsize),urcrnrlon=max(lonsize),resolution=resolution)
+    m.fillcontinents(color='gray')
+    #parallels = np.arange(0.,90,tick_int)
+    #m.drawparallels(parallels,labels=[1,0,0,0],fontsize=12)
+    # draw meridians
+    #meridians = np.arange(180.,360.,tick_int)
+    #m.drawmeridians(meridians,labels=[0,0,0,1],fontsize=12)
+    #return m,fig,(ax1,ax2)
+    return m
+
+def plot_depth(m,lons,lats,depths,depthint=[100.,200.],mode='fill'):
+    # uses FVCOM grid values
+    # where "m" is a basemap object
+    # where depth int is the depth desired
+    ''' the following might be included in main program
+    url='http://www.smast.umassd.edu:8080/thredds/dodsC/fvcom/hindcasts/30yr_gom3'
+    nc = netCDF4.Dataset(url).variables
+    lats = nc['lat'][:]
+    lons = nc['lon'][:]
+    depths = nc['h'][:]  # depth
+    '''
+    xs,ys=m(lons,lats)
+    if mode=='fill':
+        plt.tricontourf(xs,ys,depths,[200.,1000.],colors='violet',zorder=0)
+    else:
+        plt.tricontour(xs,ys,depths,[200.],linewidths=0.3,linestyles='dashed',zorder=0)
+
+def make_gif(gif_name,png_dir,frame_length = 0.2,end_pause = 4,ss=10):
     '''use images to make the gif
     frame_length: seconds between frames
     end_pause: seconds to stay on last frame
@@ -75,10 +137,11 @@ def make_gif(gif_name,png_dir,frame_length = 0.2,end_pause = 4 ):
         os.makedirs(os.path.dirname(gif_name))
     allfile_list = glob.glob(os.path.join(png_dir,'*.png')) # Get all the pngs in the current directory
     file_list=allfile_list
-    list.sort(file_list, key=lambda x: x.split('/')[-1].split('t')[0]) # Sort the images by time, this may need to be tweaked for your use case
+    #print(file_list)
+    #list.sort(file_list, key=lambda x: x.split('/')[-1].split('t')[0]) # Sort the images by time, this may need to be tweaked for your use case
     images=[]
-    # loop through files, join them to image array, and write to GIF called 'wind_turbine_dist.gif'
-    for ii in range(0,len(file_list)):       
+    # loop through files, join them to image array, and write to GIF 
+    for ii in range(0,len(file_list),ss):       
         file_path = os.path.join(png_dir, file_list[ii])
         if ii==len(file_list)-1:
             for jj in range(0,int(end_pause/frame_length)):
@@ -86,17 +149,11 @@ def make_gif(gif_name,png_dir,frame_length = 0.2,end_pause = 4 ):
         else:
             images.append(imageio.imread(file_path))
     # the duration is the time spent on each image (1/duration is frame rate)
-    imageio.mimsave(gif_name, images,'GIF',duration=frame_length)
-# loading coastlines & bathy lines
-#coastfilename='c:/users/james.manning/Downloads/basemaps/capecod_coastline_detail.csv'
-coastfilename='us_coast.dat'
-dfc=pd.read_csv(coastfilename,delim_whitespace=True,names=['lon','lat'])
-bathyfile='necs_60m.bty'
-dfb=pd.read_csv(bathyfile,delim_whitespace=True,names=['lon','lat','d1','d2'])
-dfb=dfb[dfb.lat!=0]
-dfb['lon']=dfb['lon']*-1
+    imageio.mimsave(gif_name, images,'GIF',duration=3000)#,duration=frame_length)
 
-gb= list(map(str, getgbox(area)))# returns a list of strings
+#MAIN CODE
+gbox=getgbox(area) # returns a list of corners [minlon,maxlon,minlat,maxlat]
+gb= list(map(str, gbox))# returns a list of strings
 url='http://54.208.149.221:8080/erddap/tabledap/eMOLT_RT_QAQC.csvp?tow_id%2Csegment_type%2Ctime%2Clatitude%2Clongitude%2Cdepth%2Ctemperature&segment_type=%22'+st+'%22&time%3E='+startt+'&latitude%3E='+gb[2]+'&latitude%3C='+gb[3]+'&longitude%3E='+gb[0]+'&longitude%3C='+gb[1]+''
 #url='http://54.208.149.221:8080/erddap/tabledap/eMOLT_RT_QAQC.csvp?segment_type%2Ctime%2Clatitude%2Clongitude%2Cdepth%2Ctemperature&segment_type=%22'+st+'%22&time%3E=2023-12-01T13%3A53%3A21Z&latitude%3E=41.5&latitude%3C=42.3&longitude%3E=-70.75&longitude%3C=-70.'
 df=pd.read_csv(url)
@@ -107,65 +164,57 @@ dfs.sort_values("lat", inplace=True)
 stas=dfs.tow_id.values
 dts=dfs['time (UTC)']
 dtow=dfs['tow_id']
-
+numtows=len(dtow)
 count=0
-for i in dtow:
-    if mode=='just_station_plot':
-        fig, ax1 = plt.subplots(1, 1) 
-    else:
-        fig, (ax1, ax2) = plt.subplots(1, 2)
+
+for i in dtow:#[0:1]:
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8,10))
+    plt.sca(ax1) # set the current axis
+
     #plot coast
-    ax1.plot(dfc.lon,dfc.lat,'k.',markersize=1)
-    ax1.plot(dfb.lon,dfb.lat,'g.',markersize=1)
-    ax1.text(-71.,39.,'60m isobath',color='g')
-    ax1.text(-71.,38.5,'200m isobath',color='purple')
+    m=make_basemap(gbox)
     
     # plot stations
-    ax1.plot(dfs.lon,dfs.lat,'r.',markersize=12)
+    x,y=m(dfs.lon.values,dfs.lat.values)    
+    bx=(max(x)-min(x))/20.
+    by=(max(y)-min(y))/20.
+    ax1.plot(x,y,'r.',markersize=12)
     df1=df[df['tow_id']==i]
-    ax1.plot(df1.lon.values[0],df1.lat.values[0],'k.',markersize=30)
-    ax1.set_title('eMOLT realtime',fontsize=12)
-    ax1.set_ylim(min(dfs.lat)-.1,max(dfs.lat)+.1)
-    ax1.set_xlim(min(dfs.lon)-.1,max(dfs.lon)+.1)
-    ax1.text(-71.,38.,df1.tow_id.values[0],color='k')
-    ax1.text(-74.5,44.,str(i)[:-3])
-    if mode=='just_station_plot':
-        for j in range(len(dfs)):
-            ax1.text(dfs.lon.values[j],dfs.lat.values[j],dfs.tow_id.values[j],color='k',verticalalignment='center',horizontalalignment='center',fontsize=6)
-        break
+    x1,y1=m(df1.lon.values,df1.lat.values)
+    print(y1[0])
+    ax1.plot(x1[0],y1[0],'k.',markersize=30)
+    plt.title(str(numtows)+' tows/profiles in the last 30 days of realtime eMOLT',fontsize=10)
+    if depths_down=='no':
+        plot_depth(m,lons,lats,depths,mode='isobaths')
+        ax1.set_xlabel("{:0.0f}".format(m2fth(max(df1['depth'].values)))+' fth haul w/109 fth (200m) dashed line plotted')
+
     box = ax1.get_position()
     box.x0 = box.x0 - 0.05
     box.x1 = box.x1 - 0.05
     ax1.set_position(box)
+
     # plot profiles
-    
-    #df1=df1[df1['depth']>2.0]
-    #id=np.where(np.diff(df1['depth'])<0)
-    #id=np.where(df1['depth']==max(df1['depth']))[0][0]
-    #df1=df1[0:id]#downcast
-    #ax2.plot(df1['temp'].values[id[0]],df1['depth'].values[id[0]]*-1.,'r-')
-    ax2.set_title(df1['time (UTC)'].values[0][:10]+' raw upcast in '+str(max(df1['depth'].values))+' meters')
+    ax2.set_title(df1['time (UTC)'].values[0][:10]+' raw upcast in degF')
     ax2.plot(df1['temp'].values,df1['depth'].values*-1.,'r-')
     ax2.set_ylim(-100.,0)
-    ax2.set_xlabel('tow_id '+str(df1['tow_id'].values[0])+' temp (degC)',color='r')
+    ax2.set_xlabel('tow_id '+str(df1['tow_id'].values[0])+' temp (degC)')
     ax2.set_ylabel('depth (meters)')
     
-    '''
     ax3 = ax2.twiny()
-    ax3.plot(df1['salt'].values,df1['depth'].values*-1.,'c-')
-    ax3.set_xlim(31.,36.)
-    ax3.set_title('salinity (PSU)',color='c')
-    '''
+    xl=ax2.get_xlim()
+    ax3.plot(c2f(df1['temp'].values)[0],df1['depth'].values*-1.,'m-')
+    ax3.set_xlim(c2f(xl[0])[0],c2f(xl[1])[0])
+    ax3.set_ylim(-100.,0)
+    ax4 = ax2.twinx()
+    yl=ax2.get_ylim()
+    ax4.plot(df1['temp'].values,m2fth(df1['depth'].values*-1.),'m-')
+    ax4.set_ylim(m2fth(yl[0]),m2fth(yl[1]))
+    ax4.set_ylabel('depth (fathoms)')
     count=count+1
-    #fig.savefig('plots/'+"{:03d}".format(count)+'.png')
     ib=str(i).replace(' ','_')
     ib=ib.replace(':','')
-    #fig.savefig('plots/'+ib+'.png')
-    fig.savefig('plots/'+str(count)+'.png')
+    fig.savefig('/home/user/sst/plots/'+str(count+1000)+'.png')
     plt.close(fig)
 
-if mode=='gif':
-    make_gif('gif/plot_temp_profiles.gif','/home/user/sst/plots',frame_length=2.0)
-else:
-    fig.savefig('station_plot.png')
-#eMOLT_cloud(['gif/plot_temp_profiles.gif'])
+make_gif('gif/plot_temp_profiles.gif','/home/user/sst/plots',frame_length=.2,ss=10)
+eMOLT_cloud(['gif/plot_temp_profiles.gif'])
